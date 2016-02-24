ab1809984bf9583abdc50e1af46a1bdeb8e4471c
Bennett Roesch <meta@spyrosoft.com>
2016-02-23 17:39:55 -0800

    Boilerplate.

---
+++ init.lisp
@@ -0,0 +1,21 @@
+(ql:quickload :hunchentoot)
+
+(defpackage :hexcuff
+  (:use :common-lisp :hunchentoot))
+
+(in-package :hexcuff)
+
+;; Needed if you set :error-template-directory in the easy-acceptor
+(setf hunchentoot::*show-lisp-errors-p* t)
+
+(defvar hexcuff-server
+  (make-instance 'hunchentoot:easy-acceptor
+                 :document-root "web-root"
+                 :error-template-directory "web-root/error-templates/"
+                 :access-log-destination "logs/access.log"
+                 :message-log-destination "logs/error.log"
+                 :port 8087))
+
+(load "routes.lisp")
+
+(hunchentoot:start hexcuff-server)
