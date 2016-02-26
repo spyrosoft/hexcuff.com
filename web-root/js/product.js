65bd307649b2d63c320fe5a1138b28550b057dd9
Bennett Roesch <meta@spyrosoft.com>
2016-02-25 20:09:23 -0800

    Update product price upon changing the quantity.

--- web-root/js/product.js
+++ web-root/js/product.js
@@ -6,3 +6,14 @@ function swap_main_product_image( click_event )
 	$( '.main-image' ).attr( 'src', $( this ).attr( 'src' ) );
 	$( this ).attr( 'src', swapping_img_url );
 }
+
+$( '#quantity' ).change( update_total );
+
+function update_total()
+{
+	var price = parseFloat( $( '#price' ).val() );
+	var quantity = parseInt( $( '#quantity' ).val() );
+	$( '#total' ).html( price * quantity );
+}
+
+update_total();
